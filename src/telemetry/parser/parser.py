5ad546d4da84836dc3f6950ff8f7d956be56bae3
“Teerark <teerarksith@gmail.com>
2026-04-10 16:04:22 -0500

    move method into Parser class

--- src/telemetry/parser/parser.py
+++ src/telemetry/parser/parser.py
@@ -14,16 +14,21 @@ class VariablesOffsets:
     BRAKE = 193 
     LAP_PCT = 221
 
-# TODO: implement parser
-def parseIbt (filePath: str) -> DataFrame:
-    # check if it's vaild .ibt file
-    fileType = str.split(".")[-1]
-    if(fileType != "ibt"):
-        raise ValueError("Invalid file extension: must be .ibt")
-    ir = irsdk.IBT()
-    ir.open(filePath)
+class Parser:
 
-def extractHeaderInfo():
-    session_info_len = ir._header.session_info_offset
-    buffer_size = ir._header.buf_len
-    sample_start = SESSION_INFO_OFFSET + session_info_len
+    def __init__(self):
+        ir = irsdk.IBT()
+        ir.open(filePath)
+        ir._ibt_file
+        
+    # TODO: implement parser
+    def parseIbt (filePath: str) -> DataFrame:
+        # check if it's vaild .ibt file
+        fileType = str.split(".")[-1]
+        if(fileType != "ibt"):
+            raise ValueError("Invalid file extension: must be .ibt")
+
+    def extractHeaderInfo(ir: IBT):
+        session_info_len = ir._header.session_info_offset
+        buffer_size = ir._header.buf_len
+        sample_start = SESSION_INFO_OFFSET + session_info_len
