781d270ff9a6918f1fe4a9e414919d32da1af87b
“Teerark <teerarksith@gmail.com>
2026-04-11 19:08:43 -0500

    fixed logic error, added session_info_len twice

--- src/telemetry/parser/parser.py
+++ src/telemetry/parser/parser.py
@@ -11,19 +11,41 @@ class VariablesOffsets:
     LAP_PCT = 221
 
 class Parser:
-    self.sample_start = 0
-    self.buffer_size = 0
-    self.ir
+    sample_start = 0
+    buffer_size = 0
+    brakes = []
+    throttles = []
+
     def __init__(self, filePath: str):
         # check if it's vaild .ibt file
         #TODO: veryfiy this works
-        fileType = str.split(".")[-1]
+        fileType = filePath.split(".")[-1]
         if(fileType != "ibt"):
             raise ValueError("Invalid file extension: must be .ibt")
         self.ir = irsdk.IBT()
-        ir.open(filePath)
+        self.ir.open(filePath)
 
-    def extractHeaderInfo(ir: IBT):
-        session_info_len = ir._header.session_info_offset
-        buffer_size = ir._header.buf_len
-        sample_start = SESSION_INFO_OFFSET + session_info_len
+    def extractHeaderInfo(self):
+        session_info_len = self.ir._header.session_info_len
+        self.buffer_size = self.ir._header.buf_len
+        self.sample_start = SESSION_INFO_OFFSET + session_info_len 
+    
+    def extractSpeed(self):
+        data = self.ir._ibt_file.read()
+        for i in range(1000):
+            base = self.sample_start + (i * self.buffer_size)
+
+            brake = struct.unpack_from(
+                "<f",  # little-endian float
+                data,
+                base + VariablesOffsets.BRAKE
+            )[0]
+
+            throtle = struct.unpack_from(
+                "<f",  # little-endian float
+                data,
+                base + VariablesOffsets.THROTTLE
+            )[0]
+            self.brakes.append(brake)
+            self.throttles.append(throtle)
+
