e77374946649781269959d378d993f1d3c0a17fc
“Teerark <teerarksith@gmail.com>
2026-05-13 18:18:54 -0500

    added read_float & read_int method, converted offset into a dict from a class

--- src/telemetry/parser/parser.py
+++ src/telemetry/parser/parser.py
@@ -4,17 +4,39 @@ import struct
 
 SESSION_INFO_OFFSET = 39600
 
-class VariablesOffsets:
-    SPEED = 310
-    THROTTLE = 189
-    BRAKE = 193 
-    LAP_PCT = 221
+OFFSETS = {
+    "speed": 310,
+    "throttle": 189,
+    "brake": 193,
+    "steering_angle": 185,
+    "engine_rpm": 205,
+
+    "lap_pct": 221,
+    "lap_number": 209,
+    "lap_completed": 213,
+
+    "lap_time": 245,
+    "last_lap_time": 241,
+    "best_lap_time": 237,
+
+    "lap_delta": 265,
+    "lap_delta_ok": 273,
+
+    "session_id": 20,
+    "session_length": 44
+}
+    
+
 
 class Parser:
     sample_start = 0
     buffer_size = 0
     brakes = []
     throttles = []
+    speeds = []
+    lapPcts = []
+    steeringWheelAngles =[]
+    engineRpms = []
 
     def __init__(self, filePath: str):
         # check if it's vaild .ibt file
@@ -32,23 +54,29 @@ class Parser:
         session_info_len = self.ir._header.session_info_len
         self.buffer_size = self.ir._header.buf_len
         self.sample_start = SESSION_INFO_OFFSET + session_info_len 
+
+    def read_float(data, base, key):
+        return struct.unpack_from("<f", data, base + OFFSETS[key])[0]
+
+    def read_int(data, base, key):
+        return struct.unpack_from("<d", data, base + OFFSETS[key])[0]
     
     def _parse_data(self):
         data = self.ir._ibt_file.read()
-        for i in range(1000):
+        for i in range(20000):
             base = self.sample_start + (i * self.buffer_size)
-        
-            brake = struct.unpack_from(
-                "<f",  # little-endian float
-                data,
-                base + VariablesOffsets.BRAKE
-            )[0]
 
-            throttle = struct.unpack_from(
-                "<f",  # little-endian float
-                data,
-                base + VariablesOffsets.THROTTLE
-            )[0]
+            brake = read_float(data, base, "brake")
+            throttle = read_float(data, base, "throttle")
+            speed = read_float(data, base, "speed")
+            lapPct = read_float(data, base, "lap_pct")
+            steeringWheelAngle = read_float(data, base, "steering_angle")
+            engineRpm = read_float(data, base, "engine_rpm")
+            
             self.brakes.append(brake)
             self.throttles.append(throttle)
+            self.speeds.append(speed)
+            self.lapPcts.append(lapPct)
+            self.steeringWheelAngles.append(steeringWheelAngle)
+            self.engineRpms.append(engineRpm)
 
